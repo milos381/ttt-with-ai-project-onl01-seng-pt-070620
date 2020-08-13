b11f17083ebd27e1f1bb18fb7cd3b57697dbd173
Milos Popovic <milos_popovic@outlook.com>
2020-08-13 21:52:28 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,7 @@ class Game
   [0, 4, 8],
   [6, 4, 2]
   ]
-  def initialize(board = Board.new, player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"))
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
